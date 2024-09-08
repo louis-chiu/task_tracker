fdd89292df1bd3850290ac870e4bdcd2d7a5ab83
Louis Chiu <louis.yr.chiu@gmail.com>
2024-09-08 22:03:58 +0800

    refactor: 重構 Task 功能

--- src/task_list.rs
+++ src/task_list.rs
@@ -1,24 +1,23 @@
 use super::task::{Status, Task};
 use std::fs::{self};
 use std::io::{self, ErrorKind};
-use serde::{Deserialize, Serialize};
 use serde_json;
 const TASKS_FILE: &str = "tasks.json";
 
-struct TaskList {
+pub struct TaskList {
     tasks: Vec<Task>,
     next_id: u32,
 }
 
 impl TaskList {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             tasks: Vec::<Task>::new(),
             next_id: 1,
         }
     }
 
-    pub fn build(tasks: Vec<Task>) -> Self {
+    fn build(tasks: Vec<Task>) -> Self {
         if tasks.is_empty() {
             return Self::new();
         }
@@ -29,6 +28,10 @@ impl TaskList {
         }
     }
 
+    pub fn next_id(&self) -> u32 {
+        self.next_id
+    }
+
     pub fn add(&mut self, description: &str) {
         let task = Task::new(self.next_id, description);
         self.tasks.push(task);
@@ -37,7 +40,7 @@ impl TaskList {
     }
 
     pub fn update(&mut self, id: u32, description: &str) -> bool{
-        if let Some(mut task) = self.get(id) {
+        if let Some(task) = self.get(id) {
             task.set_description(description);
             let _ = Self::write_task_list(&self.tasks);
             return true;
@@ -46,7 +49,7 @@ impl TaskList {
     }
 
     pub fn toggle_status(&mut self, status: Status, id: u32) -> bool {
-        if let Some(mut task) = self.get(id) {
+        if let Some(task) = self.get(id) {
             task.set_status(status);
             let _ = Self::write_task_list(&self.tasks);
             return true;
@@ -71,9 +74,6 @@ impl TaskList {
         self.tasks.iter().position(|task| task.id() == id)
     }
 
-    fn index_of(&self, task: Task) -> Option<usize> {
-        self.tasks.iter().position(|t| t == &task)
-    }
 
     pub fn list(&self, option: Option<&Status>) -> Vec<&Task> {
         match option {
@@ -91,18 +91,21 @@ impl TaskList {
         Ok(())
     }
 
-    pub fn read_task_list() -> Result<Self, io::Error> {
+    pub fn read_task_list() -> TaskList {
         match fs::read_to_string(TASKS_FILE) {
             Ok(contents) => {
-                let tasks: Vec<Task> = serde_json::from_str(&contents)?;
-                Ok(Self::build(tasks))
+                if let Ok(tasks) = serde_json::from_str(&contents) {
+                    Self::build(tasks)
+                } else {
+                    panic!("Error parsing tasks file...");
+                }
             }
             Err(error) => match error.kind() {
                 ErrorKind::NotFound => {
-                    fs::write(TASKS_FILE, "[]")?;
-                    Ok(Self::new())
+                    let _ = fs::write(TASKS_FILE, "[]");
+                    Self::new()
                 }
-                _ => Err(error),
+                _ => panic!("Error reading tasks file: {:?}", error),
             },
         }
     }
@@ -119,28 +122,28 @@ mod tests {
     #[test]
     fn test_first_read_task_list() {
         let _ = fs::remove_file(TASKS_FILE);
-        let tasks = TaskList::read_task_list().unwrap().tasks;
+        let tasks = TaskList::read_task_list().tasks;
         assert_eq!(tasks, Vec::<Task>::new());
     }
 
     #[test]
     fn test_add_task() {
-        let mut task_list = TaskList::read_task_list().unwrap();
+        let mut task_list = TaskList::read_task_list();
         task_list.add("testing");
-        let new_tasks = TaskList::read_task_list().unwrap();
+        let new_tasks = TaskList::read_task_list();
         assert_eq!(task_list.tasks.len(), new_tasks.tasks.len())
     }
 
     #[test]
     fn test_delete_task() {
-        let mut task_list = TaskList::read_task_list().unwrap();
+        let mut task_list = TaskList::read_task_list();
         task_list.delete(1);
         assert_eq!(task_list.tasks.len(), 0);
     }
 
     #[test]
     fn test_update_task() {
-        let mut task_list = TaskList::read_task_list().unwrap();
+        let mut task_list = TaskList::read_task_list();
         task_list.add("testing");
         task_list.update(1, "updated");
         let task = task_list.tasks.iter().find(|task| task.id() == 1).unwrap();
@@ -149,7 +152,7 @@ mod tests {
 
     #[test]
     fn test_list_tasks() {
-        let mut task_list = TaskList::read_task_list().unwrap();
+        let mut task_list = TaskList::read_task_list();
         task_list.add("testing");
         task_list.add("testing");
         task_list.add("testing");
